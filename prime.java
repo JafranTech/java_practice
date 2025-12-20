7c44e431857455a2ed1ef6da18f8fd3390ddc6fb
Jafran <mohamedrasmi86@gmail.com>
2025-12-20 20:30:59 +0530

    day 3

--- prime.java
+++ prime.java
@@ -1,8 +1,18 @@
 class prime{
     public static void main(String args[]){
         int count = 0;
-        int n=7;
+        int n=8;
 
-        for(int i =)
+        for(int i =1; i<=n;i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+        if (count == 2){
+            System.out.print("this is prime number");
+        }
+        else{
+            System.out.print("this is not prime number");
+        }
     }
 }
