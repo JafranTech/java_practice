e020f6154b1377e345a085e14fd2a1cc0bf6a5fc
Jafran <mohamedrasmi86@gmail.com>
2026-01-22 21:40:52 +0530

    final keyword

---
+++ final/final1.java
@@ -0,0 +1,11 @@
+public class final1 {
+
+    public static void main(String[] args) {
+        //if we out final in  start that cintent we cannot change!!
+        final float pi = 3.14f;
+        
+
+        System.out.println(pi);
+
+    }
+}
