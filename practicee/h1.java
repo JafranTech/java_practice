e9716a94d44f1d3e6f4d016e808b93077d245045
Jafran <mohamedrasmi86@gmail.com>
2026-02-01 21:25:59 +0530

    changes

---
+++ practicee/h1.java
@@ -0,0 +1,29 @@
+class calculator{
+    int calculate(int a , int b){
+        int sum = a+b;
+        return sum;
+    }
+
+    int calculate(int a , int b,int c){
+        int average = (a+b+c)/3;
+        return average;
+    }
+
+    double calculate(double price, double tax){
+        double final_price = price + (price*tax)/(100);
+        return final_price;
+    }
+
+}
+
+public class h1{
+    public static void main(String[] args) {
+        calculator c = new calculator();
+
+        int var =c.calculate(10, 5);
+        int var1 =c.calculate(10, 5,6);
+        int var2 =c.calculate(15000,10);
+
+        System.out.println(var + "\n" + var1 + "\n" + var2);
+    }
+}
