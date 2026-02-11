6b12e29612d9839ae9a09a23e14505f57304ea2f
Jafran <mohamedrasmi86@gmail.com>
2026-02-11 18:22:43 +0530

    pumo day 3

---
+++ pumo/increament.java
@@ -0,0 +1,12 @@
+package pumo;
+
+public class increament {
+    public static void main(String[] args) {
+        int x = 1;
+int y = ++x + x++ + ++x;
+System.out.println(y);
+System.out.println(x);
+
+
+    }
+}
