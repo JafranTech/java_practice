9a1f36e8d1ad0551fdd4967645380e71da8b9745
Jafran <mohamedrasmi86@gmail.com>
2026-02-13 21:11:22 +0530

    day 5

---
+++ static/static1.java
@@ -0,0 +1,14 @@
+public class static1 {
+    static void hello(){
+        System.out.println("i am static");
+    }
+    public static void main(String[] args) {
+        {
+            System.out.println("hii");
+        }
+
+        System.out.println("hello");
+        
+        hello();
+    }
+}
