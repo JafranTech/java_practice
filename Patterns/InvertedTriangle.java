f18553e408155661c9825f21a8b3beddc6c53336
Jafran <mohamedrasmi86@gmail.com>
2026-02-15 23:22:24 +0530

    day7

--- Patterns/InvertedTriangle.java
+++ Patterns/InvertedTriangle.java
@@ -3,11 +3,21 @@ package Patterns;
 public class InvertedTriangle {
 
     public static void main(String[] args) {
-        for(int i =4;i>=1;i--){
-            for(int j = 1; j<=i;j++){
+        // for(int i =4;i>=1;i--){
+        //     for(int j = 1; j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        int n =4;
+        for(int i =1 ; i<=n;i++){
+            for(int j =1;j<=n-i+1;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
 }
+
+
+//method 2
