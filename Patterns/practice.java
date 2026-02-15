24452c75ee009ecfc6691d4d50094bbd5ea4c0f0
Jafran <mohamedrasmi86@gmail.com>
2026-02-15 23:09:33 +0530

    day 7

--- Patterns/practice.java
+++ Patterns/practice.java
@@ -1,10 +1,13 @@
 package Patterns;
-
-public class InvertedTriangle {
-
+// ****
+// ****
+// ****
+// ****
+public class practice {
     public static void main(String[] args) {
-        for(int i =4;i>=1;i--){
-            for(int j = 1; j<=i;j++){
+        int n = 4;
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=n;j++){
                 System.out.print("*");
             }
             System.out.println();
