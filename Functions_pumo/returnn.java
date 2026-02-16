a00ae5da235b0587944d211df3472521b6656506
Jafran <mohamedrasmi86@gmail.com>
2026-02-16 16:58:24 +0530

    day8

---
+++ Functions_pumo/returnn.java
@@ -0,0 +1,18 @@
+package Functions_pumo;
+
+public class returnn {
+
+    static String hello(){
+        
+        return "hello" ;
+    }
+
+    static int age(int age){
+        return age;
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());
+
+        System.out.println(age(19));
+    }
+}
---
+++ Functions_pumo/voidd.java
@@ -0,0 +1,28 @@
+package Functions_pumo;
+
+public class voidd {
+    //without arguments
+    static void name(){
+        System.out.println("I am fazil");
+    }
+
+    static void age(int age){
+        System.out.println(age);
+        
+    }
+    public static void main(String[] args) {
+        // void- no retrun type. it is only print (sysout)
+
+        //without arguments
+       
+        name();
+
+        age(18);
+
+        
+
+
+
+
+    }
+}
