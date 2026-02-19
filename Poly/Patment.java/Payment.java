244a57704f2383b6be6b5d7bb7c2e35362ba748e
Jafran <mohamedrasmi86@gmail.com>
2026-02-19 15:10:05 +0530

    day11

--- Poly/Patment.java/Payment.java
+++ Poly/Patment.java/Payment.java
@@ -3,7 +3,16 @@ import java.util.*;
 
 class payment {
     int balance = 1500;
-    void pay() {
+
+    void pay(int amount) { // parent method must contain the sneding amount value that time only it will
+                           // work
+        /*
+         * Compiler checks reference type.
+         
+         * Runtime checks object type.
+         
+         * Parent must declare method for polymorphism to work.
+         */
 
     }
 }
@@ -11,25 +20,30 @@ class payment {
 class CreditCardPayment extends payment {
 
     @Override
-    void pay() {
+    void pay(int amount) {
         
         System.out.println("CreditCardPayment");
-        System.out.println("Balance : "+ balance);
+        balance = balance - amount;
+        System.out.println("Balance : " + balance);
     }
 
 }
 
 class UPIpayment extends payment {
     @Override
-    void pay( ) {
+    void pay(int amount) {
         System.out.println("UPIpayment");
+        balance = balance - amount;
+        System.out.println("Balance : " + balance);
     }
 }
 
 class Gpay extends payment {
     @Override
-    void pay() {
+    void pay(int amount) {
         System.out.println("Gpay");
+        balance = balance - amount;
+        System.out.println("Balance : " + balance);
     }
 }
 
@@ -44,29 +58,28 @@ public class Payment {
         int option = sc.nextInt();
         payment p;
 
-
         if (option == 1) {
             System.out.print("Enter the amount :");
-            int amount=sc.nextInt();
+            int amount = sc.nextInt();
             p = new CreditCardPayment();
-            p.pay();
-        } 
+            p.pay(amount);
+        }
 
         else if (option == 2) {
             System.out.print("Enter the amount :");
-            int amount=sc.nextInt();
+            int amount = sc.nextInt();
             p = new UPIpayment();
-            p.pay();
-        }
-        
-        else if (option ==3) {
-            System.out.print("Enter the amount :");  
-            int amount=sc.nextInt();
-            p = new UPIpayment();
-            p.pay();
+            p.pay(amount);
         }
 
-        else{
+        else if (option == 3) {
+            System.out.print("Enter the amount :");
+            int amount = sc.nextInt();
+            p = new Gpay();
+            p.pay(amount);
+        }
+
+        else {
             System.out.println("Invalid option!!1");
         }
 
