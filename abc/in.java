f2d1917e478b836281f0c55dc7a827d655f4584f
Jafran <mohamedrasmi86@gmail.com>
2026-03-18 18:06:32 +0530

    chef

---
+++ abc/in.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class in
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int fact=1;
+		int i =1;
+		
+		do while{
+		    fact = fact*i;
+		    i++;
+		}while(i<=5);
+
+	}
+}
