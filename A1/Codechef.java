17016d530c72ec0cfae1c967bfcdc27059d16831
Jafran <mohamedrasmi86@gmail.com>
2026-03-22 23:43:23 +0530

    changes

--- A1/Codechef.java
+++ A1/Codechef.java
@@ -1,3 +1,47 @@
-public class Codechef {
+// interface computer {
+//     void code();
     
+// }
+
+// class desktop implements computer{
+//     @Override
+//     public void code() {
+//         // TODO Auto-generated method stub
+//         System.out.println("desk code");
+        
+//     }
+// }
+
+// class mac implements computer{
+//     @Override
+//     public void code() {
+//         System.out.println("mac code");
+//         // TODO Auto-generated method stub
+        
+//     }
+// }
+
+// class dev {
+//    void work(computer cc,computer ccc){
+//     cc.code();
+//     ccc.code();
+//    }
+// }
+
+public class Codechef {
+
+    public static void main(String[] args) {
+
+        // computer c=new desktop();
+        // computer c1=new mac();
+        // dev d = new dev();
+        // d.work(c,c1);
+
+         int a = 15;
+    if (a > 10)
+      System.out.print("Greater than 10");
+    if (a > 5)
+      System.out.print("Greater than 5");
+        
+    }
 }
