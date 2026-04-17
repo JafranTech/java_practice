4d2d4637ee7e99d100d194ce2d6e069c38fbcef3
Jafran <mohamedrasmi86@gmail.com>
2026-04-17 17:57:42 +0530

    jaava

--- Threads/t1.java
+++ Threads/t1.java
@@ -1,22 +1,51 @@
-class A extends Thread{
-    public void run() {
-        System.out.println("one");
-    };
+class sum {
+    int count = 0;
+    public void k(){
+        count++;
+    }
 }
 
-class B extends A implements Runnable{
-    public void run(){
-        super.run();
-        System.out.println("two");
-    }
-    
-}
+
 
 public class t1 {
 
     public static void main(String[] args) {
 
-        B obj = new B();
-        obj.start();   
+        sum s = new sum();
+
+        Runnable r = () ->{
+            for(int i =1;i<=1000;i++){
+                s.k();
+            }
+        };
+
+        Runnable r1 = () ->{
+            for(int i =1;i<=1000;i++){
+                s.k();
+            }
+        };
+
+
+       
+
+        Thread t = new Thread(r);
+        Thread t2 = new Thread(r1);
+
+        t.start();
+        
+        t2.start();
+        
+        try {
+             t.join();
+        t2.join();
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+       
+        if (s.count>=1999) {
+            System.out.println(s.count);
+        }
+        
     }
 }
